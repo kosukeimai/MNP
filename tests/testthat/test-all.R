cc34142fd123749664dad48ee98c4289d431cc13
HJ08003 <hubertj@princeton.edu>
2017-09-06 14:33:11 -0400

    add more in

--- tests/testthat/test-all.R
+++ tests/testthat/test-all.R
@@ -17,9 +17,9 @@ test_that("tests MNP on the detergent data", {
               thin = 3, verbose = TRUE)
   # summarize the results
   x <- summary(res1)
-  # expect_that(length(x), is_equivalent_to(8))
-  # expect_true("coef.table" %in% names(x))
-  # expect_that(round(x$coef.table[2,2], 3), is_equivalent_to(0.115))
+  expect_that(length(x), is_equivalent_to(8))
+  expect_true("coef.table" %in% names(x))
+  expect_that(round(x$coef.table[2,2], 3), is_equivalent_to(0.115))
   # expect_that(round(x$coef.table["price", "mean"], 1), is_equivalent_to(-63.4))
   # expect_that(round(x$cov.table[2,4], 3), is_equivalent_to(0.721))
   # expect_that(round(x$cov.table["Tide:Tide", "mean"], 3), is_equivalent_to(0.658))
